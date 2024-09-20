3565d8e079d6f4836d763b52f8430db59674556b
Vineet Shenoy <vshenoy@io72>
2024-09-20 15:44:58 -0400

    pi-9: Correctly implemented cross-validation datasets for classifier training; no experiments have been run yet

--- hand_ischemia/engine/classifier_trainer.py
+++ hand_ischemia/engine/classifier_trainer.py
@@ -53,7 +53,6 @@ class Ischemia_Classifier_Trainer(SimpleTrainer):
         self.PLOT_INPUT_OUTPUT = cfg.TEST.PLOT_INPUT_OUTPUT
         self.PLOT_LAST = cfg.TEST.PLOT_LAST
         self.cls_loss = torch.nn.BCELoss()
-        self.regression_loss = CorrelationLoss()
         self.eps = 1e-6
 
         self.rank = gpu_id
@@ -185,8 +184,8 @@ class Ischemia_Classifier_Trainer(SimpleTrainer):
         Returns:
             torch.nn.Module, torch.nn.optim, torch.: The neural network modules
         """
-        model.train()
-        #cls_model.train()
+        model.eval()
+        cls_model.train()
         step = 0
         
         for i in range(0, self.epochs):
@@ -195,15 +194,17 @@ class Ischemia_Classifier_Trainer(SimpleTrainer):
             pred_labels, pred_vector, gt_labels, gt_vector = [], [], [], []
 
             for iter, (time_series, ground_truth, cls_label, window_label) in enumerate(dataloader):
-            
+                
+                
                 optimizer.zero_grad()
                 time_series = time_series.to(self.rank)
                 ground_truth = ground_truth.unsqueeze(1).to(self.rank)
                 cls_label = cls_label.to(self.rank)
                 
-                out = model(time_series.float())[:, -1:]
-                zero_mean_out = (out - torch.mean(out, axis=2, keepdim=True)) / (torch.abs(torch.mean(out, axis=2, keepdim=True)) + 1e-6) #AC-DC Normalization
-                '''
+                with torch.no_grad():
+                    out = model(time_series.float())[:, -1:]
+                    zero_mean_out = (out - torch.mean(out, axis=2, keepdim=True)) / (torch.abs(torch.mean(out, axis=2, keepdim=True)) + 1e-6) #AC-DC Normalization
+                
                 if self.CLS_MODEL_TYPE == 'SPEC':
                     
                     ################################################## Pre-processing for complex model
@@ -217,14 +218,15 @@ class Ischemia_Classifier_Trainer(SimpleTrainer):
 
                     # Running the algorithm
                     cls_out = cls_model(X)
+                
                 elif self.CLS_MODEL_TYPE == 'TiSc':
                     if zero_mean_out.shape[1] > 1: #Because the denoiser didn't collapse to one dimension
                         zero_mean_out = zero_mean_out[:, 0:1, :]
                     zero_mean_out = zero_mean_out.squeeze().float()
                     cls_out = cls_model(zero_mean_out)
-                '''
                 
-                loss = self.regression_loss(zero_mean_out, ground_truth.float()) #+ self.cls_loss(cls_out, cls_label)
+                
+                loss = self.cls_loss(cls_out, cls_label)
                 loss.backward()
                 optimizer.step()
                 
@@ -260,118 +262,7 @@ class Ischemia_Classifier_Trainer(SimpleTrainer):
         
         return cls_model, optimizer, scheduler
 
-    def train(self, experiment_id):
-        """The main training loop for the partition trainer
-
-        Args:
-            experiment_id (_type_): The MLFlow experiment ID under which to list training runs
-        """
-        
-        with open(self.train_json_path, 'r') as f:
-            train_list = json.load(f)
-        with open('/cis/net/r22a/data/vshenoy/durr_hand/model_code/physnet_ischemia/hand_ischemia/data/ubfc_only.json', 'r') as f:
-            ubfc_dict = json.load(f)
-        keys = np.array([*train_list])
-        kf = KFold(6, shuffle=False)
-        HR_nn_full, HR_gt_full = [], []
-        # Generates a partition of the data
-        for idx, (train, val) in enumerate(kf.split(keys)):
-            
-            
-            # Generating the one-versus-all partition of subjects for Hand Surgeon
-            train_subjects = keys[train]
-            val_subjects = keys[val]
-            
-            train_subdict = dict((k, train_list[k]) for k in train_subjects if k in train_list)
-            val_subdict = dict((k, train_list[k]) for k in val_subjects if k in train_list)
-
-            # Update training set with UBFC data
-            train_subdict.update(ubfc_dict) 
-            test_subject = keys[val][0]
-            logger.info('Training Fold {}'.format(test_subject))
-
-            
-            # Build dataset
-            train_dataset = H5Dataset(self.cfg, train_subdict)
-            val_dataset = H5DatasetTest(self.cfg, val_subdict)
-            logger.info('Train dataset size: {}'.format(len(train_dataset)))
-            logger.info('Test dataset size: {}'.format(len(val_dataset)))
-            
-            #Update CFG
-            self.cfg.INPUT.TRAIN_ISCHEMIC = train_dataset.num_ischemic
-            self.cfg.INPUT.TRAIN_PERFUSE = train_dataset.num_perfuse
-            self.cfg.INPUT.TEST_ISCHEMIC = val_dataset.num_ischemic
-            self.cfg.INPUT.TEST_PERFUSE = val_dataset.num_perfuse
-            #self.PLOT_INPUT_OUTPUT = False
-
-            ##Build dataloader
-            train_dataloader = DataLoader(
-                train_dataset, batch_size=self.batch_size, sampler=DistributedSampler(train_dataset))
-            val_dataloader = DataLoader(
-                val_dataset, batch_size=1, shuffle=False)
-
-            #Build the model, optimizer, and scheduler
-            model, cls_model = build_model(self.cfg)
-            model = model.to(self.rank)
-            model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
-            model = DDP(model, device_ids=[self.rank])
-            optimizer = build_optimizer(self.cfg, model)
-            lr_scheduler = build_lr_scheduler(self.cfg, optimizer)
-
-            # Create experiment and log training parameters
-            run_name = '{}'.format(test_subject)
-            config_dictionary = dict(yaml=self.cfg)
-            run = None
-            if self.rank == 0:
-                run = wandb.init(
-                    entity='vshenoy',
-                    project='hand_surgeon',
-                    group=experiment_id,
-                    name=run_name,
-                    config=config_dictionary
-                )
-                mlflow.start_run(experiment_id=experiment_id, run_name=run_name, nested=True)
-                self.log_config_dict(self.cfg)
-
-            # Train the model
-            cls_model, optimizer, lr_scheduler = self.train_partition(run, model,
-                None, optimizer, lr_scheduler, train_dataloader, val_dataloader)
-            
-            logger.warning('Finished training ')
-
-            # Test the model
-            hr_nn, hr_gt = self.test_partition(self, run, model, None, optimizer, lr_scheduler, val_dataloader, self.cfg.DENOISER.EPOCHS)
-            
-            HR_nn_full = HR_nn_full + hr_nn
-            HR_gt_full = HR_gt_full + hr_gt
-            #Compute and log the metrics; finish the run
-            met = self._compute_rmse_and_pte6(hr_gt, hr_nn)
-                        
-            mae, rmse, pte6 =  met['mae'], met['rmse'], met['pte6']
-            logger.warning('RESULTS: MAE={}; RMSE={}; PTE6={}'.format(mae, rmse, pte6))
-            
-                        
-            ## Save the Model
-            out_dir = os.path.join(self.cfg.OUTPUT.OUTPUT_DIR, test_subject)
-            os.makedirs(out_dir, exist_ok=True)
-            model_name = 'model_{}.pth'.format(test_subject)
-            
-            out_path = os.path.join(out_dir, model_name)
-            torch.save({'model_state_dict': model.module.state_dict(),
-                        'optimizer_state_dict': optimizer.state_dict()}, out_path)
-            mlflow.log_artifacts(out_dir)
-            
-            
-            if self.rank == 0:
-                mlflow.log_metrics(met, step=self.epochs)
-                mlflow.end_run()
-                run.finish() if run != None else False
-        
-        metrics = self._compute_rmse_and_pte6(HR_gt_full, HR_nn_full)
-        rmse, mae, pte6 = metrics['rmse'], metrics['mae'], metrics['pte6']
-        logger.warning('Hand Ischemia Results: MAE =  {}; RMSE = {}; PTE6 = {}'.format( mae, rmse, pte6))
-        mlflow.log_metrics(metrics, step=self.epochs)
-
+    
     
     def train_classifier(self, experiment_id, curr_exp_id):
         """The main training loop for the partition trainer
@@ -381,23 +272,49 @@ class Ischemia_Classifier_Trainer(SimpleTrainer):
         """
         with open(self.train_json_path, 'r') as f:
             train_list = json.load(f)
+        with open('/cis/home/vshenoy/durr_hand/Physnet_Ischemia/hand_ischemia/data/tourniquet_ischemia.json', 'r') as f:
+            tourniquet_list = json.load(f)
+        with open('/cis/net/r22a/data/vshenoy/durr_hand/model_code/physnet_ischemia/hand_ischemia/data/ubfc_only.json', 'r') as f:
+            ubfc_dict = json.load(f)
+        keys = np.array([*train_list])
+        tourniquet_keys = np.array([*tourniquet_list])
 
         HR_nn_full, HR_gt_full = [], []
         sub_experiments = mlflow.search_runs([experiment_id], output_format='list')
         
+        kf = KFold(6, shuffle=False)
+        HR_nn_full, HR_gt_full = [], []
         # Generates a partition of the data
-        for sub_exp in sub_experiments:
+        for idx, (train, val) in enumerate(kf.split(keys)):
+            
+            val_idx = val[0]
+            sub_exp = sub_experiments[val_idx]
+            # Generating the one-versus-all partition of subjects for Hand Surgeon
+            train_subjects = keys[train]
+            val_subjects = keys[val]
+            val_subject = val_subjects[0]
+            
+            train_tourniquet = tourniquet_keys[train]
+            val_tourniquet = tourniquet_keys[val]
             
             test_subject = sub_exp.data.tags['mlflow.runName']
             if test_subject.split('-')[0] != 'hand':
                 continue            
                         
-            #Get the training dict, except for test subject
-            train_subdict = train_list.copy()
-            del train_subdict[test_subject]
+            # Generating the one-versus-all partition of subjects for Hand Surgeon
+            train_subjects = keys[train]
+            val_subjects = keys[val]
             
-            #Get only the test subject
-            val_subdict = {test_subject: train_list[test_subject]}
+            train_tourniquet = tourniquet_keys[train]
+            val_tourniquet = tourniquet_keys[val]
+            
+            train_subdict = dict((k, train_list[k]) for k in train_subjects if k in train_list)
+            tourniquet_train_subdict = dict((k, tourniquet_list[k]) for k in train_tourniquet if k in tourniquet_list)
+            train_subdict.update(tourniquet_train_subdict)
+            
+            val_subdict = dict((k, train_list[k]) for k in val_subjects if k in train_list)
+            tourniquet_val_subdict = dict((k, tourniquet_list[k]) for k in val_tourniquet if k in tourniquet_list)
+            val_subdict.update(tourniquet_val_subdict)
 
             # Build model
             model, cls_model = build_model(self.cfg)
@@ -429,16 +346,38 @@ class Ischemia_Classifier_Trainer(SimpleTrainer):
             logger.info('Train dataset size: {}'.format(len(train_dataset)))
             logger.info('Test dataset size: {}'.format(len(val_dataset)))
 
+            
+            ## Build dataloader
+            train_dataloader = DataLoader(
+                train_dataset, batch_size=self.batch_size, sampler=DistributedSampler(train_dataset))
+            test_dataloader = DataLoader(
+                val_dataset, batch_size=1, shuffle=False)
+            
             # Create experiment and log training parameters
             run_name = '{}'.format(test_subject)
             mlflow.start_run(experiment_id=curr_exp_id,
                              run_name=run_name, nested=True)
-            self.log_config_dict(self.cfg)
 
-            '''
-            # Test the model
-            HR_nn, HR_gt = Hand_Ischemia_Trainer.test_partition(self, None, model, None, optimizer, lr_scheduler, val_dataloader, self.cfg.DENOISER.EPOCHS)
+            # Create experiment and log training parameters
+            config_dictionary = dict(
+                yaml=self.cfg,
+            )
+            run = None
+            if self.rank == 0:
+                run = wandb.init(
+                    entity='vshenoy',
+                    project='hand_surgeon',
+                    config=config_dictionary
+                )
+                mlflow.start_run(experiment_id=experiment_id,nested=True)
+                self.log_config_dict(self.cfg)
+
             
+            
+            # Train the model
+            cls_model, optimizer, lr_scheduler = self.train_partition(run, model,
+                    cls_model, optimizer, lr_scheduler, train_dataloader, test_dataloader)
+            '''
             HR_nn_full = HR_nn_full + HR_nn
             HR_gt_full = HR_gt_full + HR_gt
             
